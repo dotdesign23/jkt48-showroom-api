409d4f96ffd586d442a5fde372e740e221e071b1
dotdesign23 <61997056+dotdesign23@users.noreply.github.com>
2023-11-01 09:57:24 +0000

    Update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -22,6 +23,41 @@ app.get('/', (req, res) => {
         repository: 'https://github.com/ikhbaldwiyan/jkt48-showroom-api'
     })
 });
+
+app.get('/api/trigger', (req, res) => {
+    const channelId = "706168874525261898" // Channel ID
+    const botToken = "MTE2OTE5NzM3MzI0MzgwNTc1OA.G3QP0L.kGYcqDf5FMbgUtIH1YvoIX5OONhdaRZ1Va6-J4" // Token Bot dari Developer Discord
+
+    axios.get('https://jkt48-showroom-api-kappa.vercel.app/api/rooms').then(({ data }) => {
+        const roomData = data[0]
+
+        axios.post(`https://discord.com/api/v10/channels/${channelId}/messages`, {
+            embeds: [
+                {
+                    "type": "rich",
+                    "title": roomData.name,
+                    "description": roomData.description,
+                }],
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bot ${botToken}`,
+            },
+        }).catch(() => {
+            res.send({
+                success: false
+            })
+        });
+        
+        res.send({
+            success: true
+        })
+    }).catch(() => {
+        res.send({
+            success: false
+        })
+    })
+})
 
 app.use('/api/rooms', roomRouter);
 app.use('/api/lives', liveRouter);
